6535ff7599da014dfbab85c2a9bb6ef5fd59f005
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-09-21 03:09:41 +0300

    feat: implement JwtAuthGuard for GraphQL queries in PostResolver

--- apps/api/src/post/post.resolver.ts
+++ apps/api/src/post/post.resolver.ts
@@ -1,10 +1,14 @@
 import { Resolver, Query } from '@nestjs/graphql';
 import { PostService } from './post.service';
 import { Post } from './entities/post.entity';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/gaurds/jwt-auth/jwt-auth.guard';
 
 @Resolver(() => Post)
 export class PostResolver {
   constructor(private readonly postService: PostService) {}
+
+  @UseGuards(JwtAuthGuard)
   @Query(() => [Post], { name: 'posts' })
   findAll() {
     return this.postService.findAll();
